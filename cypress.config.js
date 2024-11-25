22e6e6bab85ab36d8ff3aa80410cc53b056e0976
Houcem Hedhili <houcemhdhili@gmail.com>
2024-11-25 18:08:03 +0100

    update config

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'https://app.papernest.com/onboarding?anonymous&anonymousId=test&id_text=1&destination=newspaper'
   },
 });
--- cypress/e2e/papernest.cy.js
+++ cypress/e2e/papernest.cy.js
@@ -2,6 +2,6 @@ describe('QA automation technical test', () => {
 
 
   it('passes', () => {
-    cy.visit('https://app.papernest.com/onboarding?anonymous&anonymousId=test&id_text=1&destination=newspaper')
+    cy.visit('/')
   })
 })
