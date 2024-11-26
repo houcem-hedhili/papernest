5e787cf6627d58bdb76fd61a35108743807b12b5
Houcem Hedhili <houcemhdhili@gmail.com>
2024-11-26 17:48:36 +0100

    finish tests

--- cypress/e2e/papernest.cy.js
+++ cypress/e2e/papernest.cy.js
@@ -1,3 +1,5 @@
+const { clientData } = require("../fixtures/client")
+
 describe('QA automation technical test', () => {
 
 
@@ -24,4 +26,30 @@ describe('QA automation technical test', () => {
   it('Offer page / “La Poste”  offers are displayed',()=>{
     cy.get('#offer_poste_6').click()
   })
-})
+
+  it('Complete clients infos page',()=>{
+    cy.get('input[id="user.email"]').type(clientData.email)
+    cy.intercept('POST', '/api/utils/email/').as('EmailValid');
+    cy.wait("@EmailValid")
+    .its("response.statusCode").should("eq", 200)
+    cy.get('input[id="user.phone_number"]').type(clientData.phone)
+    cy.get('div').contains('M.').click()
+    cy.get('input[id="user.first_name"]').type(clientData.firstname)
+    cy.get('input[id="user.last_name"]').type(clientData.lastname)
+    cy.wait(1000)
+    cy.get('#button_next').click()
+  })
+
+
+  it('Select “Je reçois le code par mail”',()=>{
+    cy.get('#text_post_office').click()
+    cy.wait(1000)
+    cy.get('#button_next').click()
+  })
+
+  it('summary of Client informations and final page',()=>{
+    cy.wait(1000)
+    cy.get('#button_next_summary').click()
+    cy.get('.title').contains("Il ne vous reste plus qu'à payer la redirection").should('be.visible')
+  })
+})  
---
+++ cypress/fixtures/client.js
@@ -0,0 +1,12 @@
+import * as client from "./client.json"
+
+const eamilprefix = Math.random().toString(36).substring(2,7);
+const email = eamilprefix+"test@papernest.com"
+export  const clientData ={
+    
+    email: email,
+    firstname: client.firstName,
+    lastname: client.lastName,
+    phone: client.phone,
+  
+}
